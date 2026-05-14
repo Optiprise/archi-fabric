bfc72110c09ba0c46b53fe18a2b71c24a2bf3e65
Bart Ratgers <bart@optiprise.nl>
2026-05-14 20:48:50 +0200

    Add jArch wrapper for docGen

---
+++ docGen/docgen.mjs
@@ -0,0 +1,7 @@
+/*
+ * @fileoverview DocGen Document Generator Script for jArchi.
+ * This script processes a selected ArchiMate view containing a document structure (groups and references)
+ * to generate a structured document, typically in PDF format via HTML.
+ * It leverages Underscore.js, custom Markup, LogBook, and Artifactory modules.
+ */
+
